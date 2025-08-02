e116a4f840dc8912296b728a81349984b279a319
NatapasL <natapas.lue@gmail.com>
2025-08-03 03:14:12 +0700

    feat: add MainLayout component and its type definitions

---
+++ src/layouts/main/MainLayout.tsx
@@ -0,0 +1,23 @@
+import { JSX } from 'react';
+
+import { HeaderBar } from '@/components/header-bar/HeaderBar';
+import { environments } from '@/configs/environments';
+import StyledComponentsRegistry from '@/lib/registry';
+
+import type { MainLayoutProps } from './types';
+
+export function LayoutMain({ children }: MainLayoutProps): JSX.Element {
+  return (
+    <StyledComponentsRegistry>
+      <main>
+        <HeaderBar
+          title="NatapasL"
+          githubUrl={environments.GITHUB_URL}
+          linkedinUrl={environments.LINKEDIN_URL}
+          email={environments.EMAIL}
+        />
+        {children}
+      </main>
+    </StyledComponentsRegistry>
+  );
+}
