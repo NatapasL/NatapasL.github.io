fb81d619748af9bff4796db63f4052eea59f46d6
NatapasL <natapas.lue@gmail.com>
2025-08-03 03:14:33 +0700

    feat: create HomePage component with styled layout

---
+++ src/screens/home/HomePage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { JSX } from 'react';
+
+import { StyledHomePage } from './styles';
+
+export function HomePage(): JSX.Element {
+  return (
+    <StyledHomePage>
+      <h1>Home</h1>
+    </StyledHomePage>
+  );
+}
---
+++ src/screens/home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import { FONT_SIZE } from '@/configs/fonts';
+
+export const StyledHomePage = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: ${FONT_SIZE.lg};
+`;
