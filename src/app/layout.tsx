44d3cc98d78bf806ef81d9c3e6a729e742289ed7
NatapasL <natapas.lue@gmail.com>
2025-08-03 16:57:44 +0700

    feat: add IBM Plex Sans Thai font and update font classes for headings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Sarabun } from 'next/font/google';
 
+import { fontClasses } from '@/configs/fonts';
 import { LayoutMain } from '@/layouts/main/MainLayout';
 
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
-const sarabun = Sarabun({
-  variable: '--font-sarabun',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -32,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${sarabun.variable}`}>
+      <body className={fontClasses}>
         <LayoutMain>{children}</LayoutMain>
       </body>
     </html>
--- src/configs/fonts.ts
+++ src/configs/fonts.ts
@@ -1,3 +1,22 @@
+import { IBM_Plex_Sans_Thai_Looped, Prompt, Sarabun } from 'next/font/google';
+
+const sarabun = Sarabun({
+  variable: '--font-sarabun',
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+});
+
+const ibmPlexSansThaiLooped = IBM_Plex_Sans_Thai_Looped({
+  variable: '--font-ibm-plex-sans-thai',
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+});
+
+const prompt = Prompt({
+  variable: '--font-prompt',
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+});
+
+export const fontClasses = [sarabun, ibmPlexSansThaiLooped, prompt].map(font => font.variable).join(' ');
+
 export const FONT_SIZE = {
   xs: 'var(--font-size-xs)',
   sm: 'var(--font-size-sm)',
