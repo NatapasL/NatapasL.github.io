e36f30ec821fa5cd0dca1fcf2451c4dc14ad72be
NatapasL <natapas.lue@gmail.com>
2025-08-04 02:15:26 +0700

    fix: update environment variable names to use NEXT_PUBLIC prefix

--- src/configs/environments.ts
+++ src/configs/environments.ts
@@ -1,8 +1,8 @@
 export const environments = {
-  GITHUB_URL: process.env.GH_URL || '',
-  LINKEDIN_URL: process.env.LINKEDIN_URL || '',
-  EMAIL: process.env.EMAIL || '',
-  SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID || '',
-  SANITY_DATASET: process.env.SANITY_DATASET || '',
-  SANITY_API_VERSION: process.env.SANITY_API_VERSION || '',
+  GITHUB_URL: process.env.NEXT_PUBLIC_GH_URL || '',
+  LINKEDIN_URL: process.env.NEXT_PUBLIC_LINKEDIN_URL || '',
+  EMAIL: process.env.NEXT_PUBLIC_EMAIL || '',
+  SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET || '',
+  SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '',
 };
