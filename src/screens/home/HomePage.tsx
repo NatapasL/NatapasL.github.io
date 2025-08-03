7e362d07be4883f87f22355c4ca8b16bba889a09
NatapasL <natapas.lue@gmail.com>
2025-08-03 14:09:48 +0700

    feat: implement HomePage component to fetch and display posts using PostCard

--- src/screens/home/HomePage.tsx
+++ src/screens/home/HomePage.tsx
@@ -1,13 +1,32 @@
 'use client';
 
-import { JSX } from 'react';
+import { JSX, useEffect } from 'react';
+import { useImmer } from 'use-immer';
 
+import { PostCard } from '@/components/post-card/PostCard';
+import { getPosts } from '@/services/post/post';
 import { StyledHomePage } from './styles';
 
+import type { PostListItem } from '@/types/post';
+
 export function HomePage(): JSX.Element {
+  const [posts, setPosts] = useImmer<PostListItem[] | undefined>(undefined);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts(): Promise<void> {
+    setPosts(await getPosts());
+  }
+
   return (
     <StyledHomePage>
-      <h1>Home</h1>
+      <div className="post-list">
+        {posts?.map(post => (
+          <PostCard key={post._id} post={post} />
+        ))}
+      </div>
     </StyledHomePage>
   );
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -4,6 +4,16 @@ import { FONT_SIZE } from '@/configs/fonts';
 
 export const StyledHomePage = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   font-size: ${FONT_SIZE.lg};
+  padding: 4px;
+
+  .post-list {
+    width: 100%;
+    max-width: 1024px;
+    row-gap: 8px;
+    display: flex;
+    flex-direction: column;
+  }
 `;
